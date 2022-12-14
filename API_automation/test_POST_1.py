9e940bdd8e7967aed293d4a82376b3ceb78ae1ca
MayurMartiwar <t_mayur.martiwar@1mg.com>
2022-12-14 18:09:54 +0530

    POST API working fine.Code is fixed now

--- API_automation/test_POST_1.py
+++ API_automation/test_POST_1.py
@@ -4,38 +4,43 @@ import requests
 import pytest
 
 
-
 def test_post_method1():
     url = "https://staginternalapi.1mg.com/finance_service/v1/tasks"
     # f = open('C:/Users/admin/Desktop/API.json', 'r')
     # json_request = json.load(f.read())
-    #
+
     # response = requests.post(url, json_request)
     # print(response.text)
-    with open('post.json') as file:
-        data = json.load(file)
-    print(data)
-    global response
+    with open('post.json', 'r') as file:
+
+        data = json.dumps(json.load(file))
+        global response
     response = requests.post(url, data)
+    print(response)
+    # test_headers(response)
+    # test_cookie(response)
+    # test_encoding(response)
+    # test_elapseTime(response)
+
     print(response.json())
+
     print(response.status_code)
     assert response.status_code == 200
 
+
 def test_headers():
-    #Fetch Response Header
+    # Fetch Response Header
     print(response.headers)
 
 def test_cookie():
-    #Fetch cookies
+    # Fetch cookies
     print(response.cookies)
 
 def test_encoding():
-    #Fetch Encoding
+    # Fetch Encoding
     print(response.encoding)
 
 def test_elapseTime():
-    #Fetch Elapsed
+    # Fetch Elapsed
     print(response.elapsed)
-
-
 
